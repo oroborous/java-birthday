6381f9c3c137c2f60f4dc12881d0172d1979597d
Stacy Read <stacy.read.007@gmail.com>
2021-02-18 19:21:53 -0600

    Exception extending throwable

--- src/edu/wctc/InvalidDateException.java
+++ src/edu/wctc/InvalidDateException.java
@@ -1,6 +1,6 @@
 package edu.wctc;
 
-public class InvalidDateException extends Throwable {
+public class InvalidDateException extends Exception {
     public InvalidDateException(int year, int month, int day) {
         super(String.format("%d/%d/%d is not a valid date.", month, day, year));
     }
